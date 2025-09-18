394923f55afb56f3d0d2bdd35867f992da63dcbf
Shirley Oduor <shirleycaren254@gmail.com>
2025-09-18 10:27:41 -0700

    added login.js file

---
+++ src/pages/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import './Login.css';
+
+function Login() {
+    const [formData, setFormData] = useState({email: "", password:""})
+
+    const handleChange = (e) => {
+        setFormData({
+            formData, [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form data submitted",formData);
+    };
+
+    return(
+       <div className="login-container">
+        <h2>Login to Your Account</h2>
+        <form onSubmit={handleSubmit} className="login-form"> 
+            <label>Email:</label>
+            <input 
+                type="email" 
+                name="email"
+                placeholder="Enter your email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+            />
+
+            <label>Password:</label>
+            <input 
+                type="password" 
+                name="password"
+                placeholder="Enter your password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+            />  
+
+            <button type="submit" className="login-button">Login</button>
+        </form>
+       </div>
+    );
+}   
+    
+export default Login;
