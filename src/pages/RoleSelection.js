1e3887ce8a35b6a59c8c4ff594616328b7fc5c44
Shirley Oduor <shirleycaren254@gmail.com>
2025-09-19 08:56:01 -0700

    added roleselection.js file

---
+++ src/pages/RoleSelection.js
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
+function RoleSelection() {
+    const navigate = useNavigate();
+
+    const handleSection = (role) => {
+        navigate(`/signup?role=${role}`);
+    };
+
+    return(
+        <div className="role-selection-container" style={{ display: 'flex', justifyContent: 'center', gap: '20px', padding: '20px' }}>
+            <p>Select your role</p>
+            <div className="role-buttons">
+                <button color="primary" onClick={() => handleSection("client")}>Client</button>
+                <button color="secondary" onClick={() => handleSection("freelancer")}>Freelancer</button>
+            </div>
+           </div>
+       );
+    }
+
+    
+export default RoleSelection;
