a1e2d5cff3c2af676ab66b6bd57c907ad3e8b64e
Shirley Oduor <shirleycaren254@gmail.com>
2025-09-19 12:22:33 -0700

    added CTA.js file

---
+++ src/components/layout/CTA.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function CTA() {
+    return(
+        <section className="cta">
+            <h2>Ready to get started?</h2>
+            <p>Join Syncley today and take your projects to the next level!</p>
+            
+            <button className="cta-button">Get Started</button>
+
+        </section>
+    );
+}
+
+export default CTA;
