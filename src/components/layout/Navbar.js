3e361dd9a3cb962e59de253900b77e5764c7ff28
Shirley Oduor <shirleycaren254@gmail.com>
2025-09-29 00:59:35 -0700

    modified files

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -8,6 +8,7 @@ function Navbar() {
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
+    <>
     <nav className="st-navbar">
       <div className="st-navbar-logo">Syncley</div>
 
@@ -20,15 +21,19 @@ function Navbar() {
 
       <div className={`st-navbar-links ${isOpen ? "active" : ""}`}>
         <Link to="/" className="st-navbar-link" onClick={() => setIsOpen(false)}>Home</Link>
-        <Link to="/about" className="st-navbar-link" onClick={() => setIsOpen(false)}>Features</Link>
-        <Link to="/contact" className="st-navbar-link" onClick={() => setIsOpen(false)}>Boards</Link>
-        <Link to="/findfreelancers" className="st-navbar-link" onClick={() => setIsOpen(false)}>Inspiration</Link>
-        <Link to="/findwork" className="st-navbar-link" onClick={() => setIsOpen(false)}>About</Link>
+        <Link to="/features" className="st-navbar-link" onClick={() => setIsOpen(false)}>Why Syncley</Link>
+        <Link to="/board" className="st-navbar-link" onClick={() => setIsOpen(false)}>Board</Link>
+       {/*S<Link to="/inspiration" className="st-navbar-link" onClick={() => setIsOpen(false)}>Inspiration</Link>*/}
+        {/*<Link to="/about" className="st-navbar-link" onClick={() => setIsOpen(false)}>About</Link>*/}
         <Link to="/login" className="st-navbar-link nav-btn" onClick={() => setIsOpen(false)}>Login</Link>
         <Link to="/signup" className="st-navbar-link nav-btn-primary" onClick={() => setIsOpen(false)}>Sign Up</Link>
       </div>
     </nav>
+
+     <div className={`mobile-overlay ${isOpen ? "active" : ""}`} onClick={toggleMenu}></div>
+    </>
+    
   );
-}
+} 
 
 export default Navbar;
