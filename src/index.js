b036e1bec2b14c8d9c35508b0f6cc3a563dd5cc1
Shirley Oduor <shirleycaren254@gmail.com>
2025-09-29 00:49:56 -0700

    modified files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '@fontsource/inter/500.css';
+import '@fontsource/montserrat/700.css';
+import '@fontsource/open-sans/400.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
